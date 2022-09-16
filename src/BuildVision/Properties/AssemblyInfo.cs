47e3974b389115b8c0441c3798f3d7c974a51370
Sören Kuklau <chucker23n+github@gmail.com>
2022-09-16 22:49:02 +0200

    modernize project

--- src/BuildVision/Properties/AssemblyInfo.cs
+++ src/BuildVision/Properties/AssemblyInfo.cs
@@ -2,18 +2,5 @@
 using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("BuildVision")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("BuildVision")]
-[assembly: AssemblyProduct("BuildVision")]
-[assembly: AssemblyCopyright("© 2017 BuildVision")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]   
 [assembly: ComVisible(false)]     
 [assembly: CLSCompliant(false)]
-[assembly: NeutralResourcesLanguage("en-US")]
